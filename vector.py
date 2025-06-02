ddd1b5c705b238bb837bcb5b04410c3a65e1b2e0
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-06-02 11:01:25 +0200

    vector database

--- vector.py
+++ vector.py
@@ -23,4 +23,11 @@ if add_documents:
         ids.append(str(i))
         documents.append(document) 
         
-vector_
+vector_store= Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
